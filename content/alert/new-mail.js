b7075aa145da6d9a3e99fd5ed106a96ae963fad5
unnecessarydave <161469690+unnecessarydave@users.noreply.github.com>
2025-01-03 15:21:18 +0000

    remove link to Inbox in NewMail

--- content/alert/new-mail.js
+++ content/alert/new-mail.js
@@ -49,16 +49,6 @@ Foxtrick.modules['NewMail'] = {
 		if (myHt.getElementsByTagName('span').length) {
 			let mailCountSpan = myHt.getElementsByTagName('span')[0];
 			mailCountSpan.className = 'ft-new-mail';
-
-			Foxtrick.onClick(mailCountSpan, function() {
-				let doc = this.ownerDocument;
-				let newURL = new URL('/MyHattrick/Inbox/', doc.location.origin);
-				doc.location.assign(newURL);
-
-				// disable MyHT link
-				return false;
-			});
-
 			newMailCount = parseInt(mailCountSpan.textContent.match(/\d+/)[0], 10) || 0;
 		}
 
