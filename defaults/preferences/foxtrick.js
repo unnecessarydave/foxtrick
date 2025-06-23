f82b49212a6ecac34fb6a30bbbefd836a9f0dbc7
unnecessarydave <161469690+unnecessarydave@users.noreply.github.com>
2025-06-23 20:22:15 +0100

    disable FlagCollectionToMap pending removal

--- defaults/preferences/foxtrick.js
+++ defaults/preferences/foxtrick.js
@@ -104,7 +104,7 @@ pref("extensions.foxtrick.prefs.module.FixcssProblems.enabled", true);
 pref("extensions.foxtrick.prefs.module.FixcssProblems.MatchReportRatingsFontFix.enabled", false);
 pref("extensions.foxtrick.prefs.module.FixcssProblems.RemoveForumSneakPeak.enabled", false);
 pref("extensions.foxtrick.prefs.module.FixcssProblems.RTL_Fixes.enabled", false);
-pref("extensions.foxtrick.prefs.module.FlagCollectionToMap.enabled", true);
+pref("extensions.foxtrick.prefs.module.FlagCollectionToMap.enabled", false);
 pref("extensions.foxtrick.prefs.module.FormatPostingText.enabled", true);
 pref("extensions.foxtrick.prefs.module.FormatPostingText.NestedQuotesAsSpoilers.enabled", true);
 pref("extensions.foxtrick.prefs.module.FormatPostingText.NestedQuotesAsSpoilers_text", "3");
